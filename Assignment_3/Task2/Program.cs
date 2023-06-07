cff02a307d519441f1b86ddb4fb2408fd6b4ec72
Gig Baidoshvili <g.baidoshvili@gmail.com>
2023-06-07 18:09:59 +0400

    first commit

---
+++ Assignment_3/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+var InputtString = Console.ReadLine();
+
+if(InputtString == null)
+{
+    Console.WriteLine("Invalid input");
+    return;
+}
+
+var InputInt = Convert.ToInt64((string)InputtString);
+
+int DivisorCount = 0;
+
+for (int i = 1; i <= Math.Sqrt(InputInt); i++)
+{
+    DivisorCount += (InputInt % i == 0)?((InputInt/i==i)?1:2):0;
+}
+
+
+var Output = "This number has " + DivisorCount + " divisors";
+
+Console.WriteLine(Output);
