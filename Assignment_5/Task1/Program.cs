634bcf11125dc76b9ee2bdebd7f795782c22736e
Gig Baidoshvili <g.baidoshvili@gmail.com>
2023-06-12 17:10:33 +0400

    assignment 5

---
+++ Assignment_5/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿static long SumDigits(long inputNumber)
+{
+    long sum = 0, digit = 0;
+
+    while (inputNumber > 0)
+    {
+        digit = inputNumber % 10;
+        sum = sum + digit;
+        inputNumber = inputNumber / 10;
+    }
+
+    return sum;
+}
+
+
+static long ArrayElementDigitSum(int[] inputArray, int inputInt)
+{
+    return SumDigits(inputArray[inputInt]);
+}
+
+
+Console.WriteLine(ArrayElementDigitSum(new int[] {1561, 84949, 894984, 151891, 84998, 168841}, 5));
---
+++ Assignment_5/Task2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Concurrent;
+
+static char[] CreateArray()
+{
+    Console.WriteLine("Input array length");
+
+    int arrayLength = Convert.ToInt32(Console.ReadLine());
+
+    char[] array = new char[arrayLength];
+
+    for (int i = 0; i < arrayLength; i++)
+    {
+        Console.WriteLine("input element number " + i);
+        array[i] = Convert.ToChar(Console.ReadLine());
+    }
+
+    return array;
+}
+
+static int CountElementInArray(char[] array, char symbol)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++) 
+    {
+        if (array[i] == symbol) count++;
+    }
+    return count;
+}
+
+static void OutputResult(char symbol, int count)
+{
+    Console.WriteLine(symbol+" was fount in the array "+count+" times");
+}
+
+
+char[] inputArray = CreateArray();
+
+Console.WriteLine("Which element do you want to count in this array?");
+
+char elementToCount = Convert.ToChar(Console.ReadLine());
+
+OutputResult(elementToCount, CountElementInArray(inputArray, elementToCount));
+
+
+
